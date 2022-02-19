d03d91e3693954a905530a404631e43c7e0fff9f
Pankaj <pankaj.jha1404@gmail.com>
2022-02-20 03:45:28 +0530

    implementation of context provider and consumer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    const storedUserLoggedInInformation =
+      localStorage.getItem('isLoggedIn');
 
     if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
@@ -28,13 +30,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+      <MainHeader
+        // isAuthenticated={isLoggedIn}
+        onLogout={logoutHandler}
+      />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/components/store/auth-context.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+});
+export default AuthContext;
